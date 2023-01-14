3af59b5a1a6157c20c8c47ef47460d5b8e824bd6
AngelBozhiy <122650756+AngelBozhiy@users.noreply.github.com>
2023-01-14 06:26:58 +0200

    Create Player.java

---
+++ Lab3/Player.java
@@ -0,0 +1,26 @@
+public class Player {
+    String name;
+    Player(String name)
+    {
+        this.name = name;
+    }
+    int wins;
+    int loses;
+    String name()
+    {
+        return name;
+    }
+    String info()
+    {
+        return "Name: "+name+", Wins: "+wins+", Loses: "+loses;
+    }
+    void win()
+    {
+        System.out.println(name()+" win!");
+        wins++;
+    }
+    void lose()
+    {
+        loses++;
+    }
+}
