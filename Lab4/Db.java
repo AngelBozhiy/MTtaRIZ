aa700ce64028a693680281a06d9226250fa1f7d4
Костишен Дмитро <122650756+AngelBozhiy@users.noreply.github.com>
2023-01-23 00:27:05 +0200

    Update Db.java

--- Lab4/Db.java
+++ Lab4/Db.java
@@ -1,60 +1,47 @@
 import java.sql.*;
 
 class Db {
-    String dbUrl = "jdbc:mysql://localhost:3306/myGame?useSSL=false";
+    String dbUrl = "jdbc:mysql://localhost:3306/myGame?allowPublicKeyRetrieval=true&useSSL=false";
     String user = "root";
-    String password = "Root";
-    Connection connect;
+    String password = "NikNik79NikNik79";
+    Connection con;
 
-    public Db() 
-    {
-        try 
-        {
+    public Db() {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            this.connect = DriverManager.getConnection(dbUrl, user, password);
-        } catch (Exception e) 
-        {
+            this.con = DriverManager.getConnection(dbUrl, user, password);
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public void close() 
-    {
-        try 
-        {
-            connect.close();
-        } catch (Exception e) 
-        {
+    public void close() {
+        try {
+            con.close();
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public boolean isUserExists(String username) 
-    {
-        try 
-        {
-            Statement stmt = connect.createStatement();
+    public boolean isUserExists(String username) {
+        try {
+            Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT count(*) FROM users WHERE username='" + username + "';");
             while (rs.next()) if (rs.getInt(1) == 1) return true;
             else return false;
-        } 
-        catch (Exception e) 
-        {
+        } catch (Exception e) {
             System.out.println(e);
         }
         return true;
     }
-    public boolean isUserPasswordExists(String username, String password) 
-    {
-        try 
-        {
-            Statement stmt = connect.createStatement();
+    public boolean isUserPasswordExists(String username, String password) {
+        try {
+            Statement stmt = con.createStatement();
             ResultSet rsPass = stmt.executeQuery("SELECT password FROM users WHERE username='" + username + "';");
             while (rsPass.next()) if (rsPass.getString(1) .equals(password))
                 return true;
             else return false;
-        } catch (Exception e) 
-        {
+        } catch (Exception e) {
             System.out.println(e);
         }
         return false;
